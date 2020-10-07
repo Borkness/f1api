e49ccd8937a9091edab02604ccc31d2ad8416501
Andy Williams <andy@zipperty.com>
2020-10-07 20:29:44 +0100

    Update results model to add missing GeneratedValue annotation

--- src/main/kotlin/com/borkness/f1api/models/Results.kt
+++ src/main/kotlin/com/borkness/f1api/models/Results.kt
@@ -7,6 +7,7 @@ import javax.persistence.*
 data class Results (
     @Id
     @Column(name = "resultid")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id : Long,
     @Column(name = "raceid")
     val raceId : Int,
