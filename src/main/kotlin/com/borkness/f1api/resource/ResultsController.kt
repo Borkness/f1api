506b5dafc8087d760939f17103c71758ecdf04de
Andy Williams <andy@zipperty.com>
2020-10-08 22:08:55 +0100

    Add pagination to getAllResults in result controller

--- src/main/kotlin/com/borkness/f1api/resource/ResultsController.kt
+++ src/main/kotlin/com/borkness/f1api/resource/ResultsController.kt
@@ -2,24 +2,26 @@ package com.borkness.f1api.resource
 
 import com.borkness.f1api.models.Results
 import com.borkness.f1api.repository.ResultsRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("api/v1/results")
 class ResultsController(private val resultsRepository: ResultsRepository) {
 
     @GetMapping
-    fun getAllResults() : ResponseEntity<List<Results>> {
-        val results : List<Results> = resultsRepository.findAll().map { results ->
+    fun getAllResults(@RequestParam(defaultValue = "0") page : Int = 0,
+                      @RequestParam(defaultValue = "25") pageSize : Int = 25) : ResponseEntity<List<Results>> {
+        val pagination : PageRequest = PageRequest.of(page, pageSize)
+
+        val results : Page<Results> = resultsRepository.findAll(pagination).map { results ->
             results.add(linkTo(methodOn(this.javaClass).getAllResults()).withSelfRel())
         }
-        return ResponseEntity.ok(results)
+        return ResponseEntity.ok(results.content)
     }
 
     @GetMapping("/{id}")
