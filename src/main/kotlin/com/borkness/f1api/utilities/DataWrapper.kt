03bdfa8ea581eb26b8e87fe5f4ba9dc7333301fc
Andy Williams <andy@zipperty.com>
2020-10-09 21:07:15 +0100

    Add doc block to data wrapper class

--- src/main/kotlin/com/borkness/f1api/utilities/DataWrapper.kt
+++ src/main/kotlin/com/borkness/f1api/utilities/DataWrapper.kt
@@ -2,6 +2,14 @@ package com.borkness.f1api.utilities
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
+/**
+ * Data wrapper
+ *
+ * Wraps a Json Resource with a key returning a list.
+ *
+ * @param T the type of resource
+ * @property data
+ */
 class DataWrapper<T>(data : T) {
     private var data : List<T> = emptyList()
     init {
